14dbc2a6af0fcab679023fa1ee1feab171ba7f48
Jay Harris <jaywritescode@users.noreply.github.com>
2019-12-27 21:12:01 -0500

    documentation

--- src/main/java/info/jayharris/minimax/search/CutoffTest.java
+++ src/main/java/info/jayharris/minimax/search/CutoffTest.java
@@ -6,6 +6,13 @@ import info.jayharris.minimax.State;
 
 import java.util.function.Predicate;
 
+/**
+ * A predicate that is applied to a node in a search tree, and that returns
+ * true iff the search should be terminated at that node.
+ *
+ * @param <S> a game state
+ * @param <A> an action taken in the game
+ */
 public abstract class CutoffTest<S extends State<S, A>, A extends Action<S, A>> implements Predicate<Node<S, A>> {
 
     public boolean cutoffSearch(Node<S, A> node) {
